d5a3d28ca1e496b580ee11867cca4415cf170db8
GiwMan <giorgosmantzouranes@gmail.com>
2022-10-01 01:03:09 +0300

    get all auctions for a user

--- back-end/src/routes/item.route.js
+++ back-end/src/routes/item.route.js
@@ -7,6 +7,7 @@ const itemController = require('../controllers/item.controller');
 
 router.post("/",itemController.storeItem);
 router.get("/:userId",itemController.getItems);
+router.get("/", itemController.getAllItems);
 router.post("/:itemId/bid",itemController.storeBid);
 router.post("/:itemId/buy",itemController.buyItem);
 router.delete("/:itemId", itemController.deleteItem);
