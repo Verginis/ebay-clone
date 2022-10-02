6cb93ff8a63364a18f96101d0d5bd7d6925376cd
Verginis <averginis@threenitas.com>
2022-10-02 23:51:31 +0300

    auction

--- back-end/config/db.config.js
+++ back-end/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD: "1a2r3i4s@",
+    PASSWORD: "auction-website-clone",
     DB: "user",
     dialect: "mysql",
     pool: {
